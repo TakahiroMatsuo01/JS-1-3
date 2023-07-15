5b7e3289b6e1eebe9336fac833d4150a20ecdd97
codewriterX <miturugi.infinite@gmail.com>
2023-07-15 16:34:52 +0900

    修正前

--- JS/main.js
+++ JS/main.js
@@ -1,35 +1,27 @@
-const arry_title=[
-    '===============================',
-    '現在持っているタスク一覧',
-    '==============================='
-]
+const Title = '===============================\n現在持っているタスク一覧\n===============================';
 
-const arry_task=[
+const Tasks = [
     "掃除",'買い物','散歩'
 ]
 
 const add = function(){
-    arry_title.forEach((value)=> {
-        console.log(value);
-    });
+    console.log(Title);
 
-    arry_task.forEach((value,index )=> {
+    Tasks.forEach((value,index ) => {
         console.log(index+":"+value);
     });
 
-    const input_1=prompt("「確認、追加、削除、終了のいずれかを入力してください。」");
-    const input_2=prompt("タスクを入力してください。");
+    const comformDialog = prompt("「確認、追加、削除、終了のいずれかを入力してください。」");
+    const inputDialog=prompt("タスクを入力してください。");
 
-    if(input_2 !==''){
-        arry_task.push(input_2);
+    comformDialog;
+    Tasks.push(inputDialog);
 
-        arry_title.forEach((value)=> {
-            console.log(value);
-        });
-        
-        arry_task.forEach((value,index )=> {
-            console.log(index+":"+value);
-        });
-    }
+    Title;
+    
+    Tasks.forEach((value,index )=> {
+        console.log(index+':'+value);
+    });   
 }
+
 add();
