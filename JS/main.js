2026b324573d28ea7c3695ff5c38135a5a4137da
codewriterX <miturugi.infinite@gmail.com>
2023-07-20 20:41:51 +0900

    修正後

--- JS/main.js
+++ JS/main.js
@@ -1,25 +1,25 @@
-const Title = '===============================\n現在持っているタスク一覧\n===============================';
+const titleSet = '===============================\n現在持っているタスク一覧\n===============================';
 
-const Tasks = [
-    "掃除",'買い物','散歩'
+const tasksList = [
+    '掃除','買い物','散歩'
 ]
 
 const add = function(){
-    console.log(Title);
+    console.log(titleSet);
 
-    Tasks.forEach((value,index ) => {
-        console.log(index+":"+value);
+    tasksList.forEach((value,index ) => {
+        console.log(index+':'+value);
     });
 
-    const comformDialog = prompt("「確認、追加、削除、終了のいずれかを入力してください。」");
-    const inputDialog=prompt("タスクを入力してください。");
+    const comformDialog = prompt('「確認、追加、削除、終了のいずれかを入力してください。」');
+    const inputDialog=prompt('タスクを入力してください。');
 
     comformDialog;
-    Tasks.push(inputDialog);
+    tasksList.push(inputDialog);
 
-    Title;
+    titleSet;
     
-    Tasks.forEach((value,index )=> {
+    tasksList.forEach((value,index )=> {
         console.log(index+':'+value);
     });   
 }
